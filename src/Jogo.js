8a0bab490c83de549415bcfc7ab7f408e3ee5eb0
Carlos Bolzanell <carlosbolzanell@gmail.com>
2023-09-26 21:52:48 -0300

    Jogo da velha

--- src/Jogo.js
+++ src/Jogo.js
@@ -4,43 +4,88 @@ import { StyleSheet, View, Text} from "react-native";
 import { Button } from "react-native";
 
 export default function Jogo(props){
-
-    let contador = "1";
-
-     const [b1, setB1] = useState("");
-
-    const handleClickGame = () =>{
-        if(contador%2 === 0){
-            setB1("X");
+    let contador = 1;
+    let jogadorDaVez = 'Jogador 1';
+    
+    let setVez = () =>{
+        if(contador%2 == 0){
+            jogadorDaVez = "Jogador 1"
             contador++
-        }else{
-            setB1("O");
-            contador++;
+            return 1
+        }else if(contador%2 == 1){
+            jogadorDaVez = "Jogador 2"
+            return 2
+            contador++
         }
+        return "";
+    } 
+
+    let marcador = ()=>{
+        if(setVez() == 1){
+            return 'X';
+        }else if(setVez == 2){
+            return 'O';
+        }
+        return " "
     }
 
-    const handleClick = ()=>{
-        props.changeScream('home');
+
+    const [b1, setB1] = useState("");
+    const [b2, setB2] = useState("");
+    const [b3, setB3] = useState("");
+    const [b4, setB4] = useState("");
+    const [b5, setB5] = useState("");
+    const [b6, setB6] = useState("");
+    const [b7, setB7] = useState("");
+    const [b8, setB8] = useState("");
+    const [b9, setB9] = useState("");
+
+    const handleClickB1 = () =>{
+        setB1(marcador());
+    }
+    const handleClickB2 = () =>{
+        setB2(marcador());
+    }
+    const handleClickB3 = () =>{
+        setB3(marcador());
+    }
+    const handleClickB4 = () =>{
+        setB4(marcador());
+    }
+    const handleClickB5 = () =>{
+        setB5(marcador());
+    }
+    const handleClickB6 = () =>{
+        setB6(marcador());
+    }
+    const handleClickB7 = () =>{
+        setB7(marcador());
+    }
+    const handleClickB8 = () =>{
+        setB8(marcador());
+    }
+    const handleClickB9 = () =>{
+        setB9(marcador());
     }
 
     return(
         <View style={styles.container}>
             <StatusBar style="auto" />
-            <Text style={styles.title}>Jogo</Text>
+            <Text style={styles.title}>Vez de {jogadorDaVez}</Text>
             <View style={styles.col1}>
-                <Button title = {b1} style={styles.btn} onPress={handleClickGame} />
-                <Button title = {b1} style={styles.btn}/>
-                <Button title = {b1} style={styles.btn}/>
+                <Button title = {b1} style={styles.btn} onPress = {handleClickB1}/>
+                <Button title = {b2} style={styles.btn} onPress = {handleClickB2}/>
+                <Button title = {b3} style={styles.btn} onPress = {handleClickB3}/>
             </View>
             <View style={styles.col1}>
-                <Button title = {b1} style={styles.btn}/>
-                <Button title = {b1} style={styles.btn}/>
-                <Button title = {b1} style={styles.btn}/>
+                <Button title = {b4} style={styles.btn} onPress = {handleClickB4}/>
+                <Button title = {b5} style={styles.btn} onPress = {handleClickB5} />
+                <Button title = {b6} style={styles.btn} onPress = {handleClickB6}/>
             </View>
             <View style={styles.col1}>
-                <Button title = {b1} style={styles.btn}/>
-                <Button title = {b1} style={styles.btn}/>
-                <Button title = {b1} style={styles.btn}/>
+                <Button title = {b7} style={styles.btn} onPress = {handleClickB7}/>
+                <Button title = {b8} style={styles.btn} onPress = {handleClickB8}/>
+                <Button title = {b9} style={styles.btn} onPress = {handleClickB9}/>
             </View>
         </View>
     )
